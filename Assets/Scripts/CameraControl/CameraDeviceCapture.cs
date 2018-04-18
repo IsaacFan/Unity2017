e226356b0bfc06b0928b7789f2b6c867c32dec14
Isaac <a1107123@hotmail.com>
2018-04-18 19:44:19 +0100

    add video capture by phsical camera

--- Assets/Scripts/CameraControl/CameraDeviceCapture.cs
+++ Assets/Scripts/CameraControl/CameraDeviceCapture.cs
@@ -1,299 +1,45 @@
 ﻿using System.Collections;
-using System.Linq;
+//using System.Linq;
 using UnityEngine;
-using UnityEngine.XR.WSA.WebCam;
+//using UnityEngine.XR.WSA.WebCam;
 
 public class CameraDeviceCapture : MonoBehaviour
 {
-    static readonly float MaxRecordingTime = 5.0f;
 
-    VideoCapture m_VideoCapture = null;
-    float m_stopRecordingTimer = float.MaxValue;
+    private WebCamTexture physicalWebCamTexture;
+
+    public Renderer displayRenderer;
+
 
     // Use this for initialization
     void Start()
     {
-        Debug.Log("SSS");
 
-        VideoCapture.CreateAsync(true, OnVideoCaptureCreated);
-        //PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
-
-        //Debug.Log(Everyplay.read.ReadyForRecording());
-
-        //Everyplay.StartRecording();
-        //StartVideoCaptureTest();
-    }
-    void OnVideoCaptureCreated(VideoCapture videoCapture)
-    {
-
-        Debug.Log("WWW");
-
-        if (videoCapture != null)
-        {
-            Debug.Log("SSS");
-
-            m_VideoCapture = videoCapture;
-
-            Resolution cameraResolution = VideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-            float cameraFramerate = VideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
-
-            CameraParameters cameraParameters = new CameraParameters();
-            cameraParameters.hologramOpacity = 0.0f;
-            cameraParameters.frameRate = cameraFramerate;
-            cameraParameters.cameraResolutionWidth = cameraResolution.width;
-            cameraParameters.cameraResolutionHeight = cameraResolution.height;
-            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-
-            m_VideoCapture.StartVideoModeAsync(cameraParameters,
-                                                VideoCapture.AudioState.None,
-                                                OnStartedVideoCaptureMode);
-        }
-        else
-        {
-            Debug.LogError("Failed to create VideoCapture Instance!");
-        }
-    }
-
-
-
-
-    /*
-    PhotoCapture photoCaptureObject = null;
-    void OnPhotoCaptureCreated(PhotoCapture captureObject)
-    {
-
-        Debug.Log("OO");
-
-        photoCaptureObject = captureObject;
-
-        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-
-        CameraParameters c = new CameraParameters();
-        c.hologramOpacity = 0.0f;
-        c.cameraResolutionWidth = cameraResolution.width;
-        c.cameraResolutionHeight = cameraResolution.height;
-        c.pixelFormat = CapturePixelFormat.BGRA32;
-
-        captureObject.StartPhotoModeAsync(c, false, OnPhotoModeStarted);
-
-
-    }
-    private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
-    {
-        if (result.success)
-        {
-            string filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
-            string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-
-            photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
-        }
-        else
-        {
-            Debug.LogError("Unable to start photo mode!");
-        }
-    }
-    */
-
-
-
-
-    void Update()
-    {
-        if (m_VideoCapture == null || !m_VideoCapture.IsRecording)
-        {
+        WebCamDevice[] webCamDevices = WebCamTexture.devices;
+        if (webCamDevices.Length == 0)
             return;
-        }
 
-        if (Time.time > m_stopRecordingTimer)
-        {
-            m_VideoCapture.StopRecordingAsync(OnStoppedRecordingVideo);
-        }
+        StartCoroutine(openCamera(webCamDevices[0].name));
     }
 
-    void StartVideoCaptureTest()
+    private IEnumerator openCamera(string webCamDeviceName)
     {
-        Resolution cameraResolution = VideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-        Debug.Log(cameraResolution);
-
-        float cameraFramerate = VideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
-        Debug.Log(cameraFramerate);
-
-        VideoCapture.CreateAsync(false, delegate (VideoCapture videoCapture)
-        {
-            Debug.Log("XXX");
-
-            if (videoCapture != null)
-            {
-                m_VideoCapture = videoCapture;
-                Debug.Log("Created VideoCapture Instance!");
-
-                CameraParameters cameraParameters = new CameraParameters();
-                cameraParameters.hologramOpacity = 0.0f;
-                cameraParameters.frameRate = cameraFramerate;
-                cameraParameters.cameraResolutionWidth = cameraResolution.width;
-                cameraParameters.cameraResolutionHeight = cameraResolution.height;
-                cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-
-                m_VideoCapture.StartVideoModeAsync(cameraParameters,
-                VideoCapture.AudioState.ApplicationAndMicAudio,
-                OnStartedVideoCaptureMode);
-            }
-            else
-            {
-                Debug.LogError("Failed to create VideoCapture Instance!");
-            }
-        });
-    }
-
-    void OnStartedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Started Video Capture Mode!");
-        string timeStamp = Time.time.ToString().Replace(".", "").Replace(":", "");
-        string filename = string.Format("TestVideo_{0}.mp4", timeStamp);
-        string filepath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-        filepath = filepath.Replace("/", @"\");
-        m_VideoCapture.StartRecordingAsync(filepath, OnStartedRecordingVideo);
-    }
-
-    void OnStoppedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Stopped Video Capture Mode!");
-    }
-
-    void OnStartedRecordingVideo(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Started Recording Video!");
-        m_stopRecordingTimer = Time.time + MaxRecordingTime;
-    }
-
-    void OnStoppedRecordingVideo(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Stopped Recording Video!");
-        m_VideoCapture.StopVideoModeAsync(OnStoppedVideoCaptureMode);
-    }
-}
-
-
-/*
-public class CameraDeviceCapture : MonoBehaviour {
-
-    static readonly float MaxRecordingTime = 5.0f;
-
-    private VideoCapture videoCapture;
-    float m_stopRecordingTimer = float.MaxValue;
-
-    // Use this for initialization
-    void Start () {
-
-
-
-
-        //startVideoCapture();
-
-
-        StartCoroutine(open_Camera());//開啟攝影機鏡頭
-
-
-    }
-
-
-    IEnumerator open_Camera()
-    {
-        //授權開啟鏡頭
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam) == true)
         {
-            
-            //設置攝影機要攝影的區域
-            //(攝影機名稱, 攝影機要拍到的寬度, 攝影機要拍到的高度, 攝影機的FPS)
-            //WebCamTexture myCam = new WebCamTexture(WebCamTexture.devices[0].name, Screen.width, Screen.height, 60);
-            //rimgShow.texture = myCam;
-            //myCam.Play();//開啟攝影機
-
-            //Debug.Log("Open");
-            
-            startVideoCapture();
+            physicalWebCamTexture = new WebCamTexture(webCamDeviceName);
+            displayRenderer.material.mainTexture = physicalWebCamTexture;
+            physicalWebCamTexture.Play();
         }
     }
 
-
-    // Update is called once per frame
-    void Update () {
-
-        if (videoCapture == null || !videoCapture.IsRecording)
-        {
-            return;
-        }
-
-        if (Time.time > m_stopRecordingTimer)
-        {
-            videoCapture.StopRecordingAsync(OnStoppedRecordingVideo);
-        }
-    }
-
-
-    private void startVideoCapture()
+    private void onClickSavePictureButton()
     {
-        Resolution cameraResolution = VideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-        Debug.Log(cameraResolution);
-
-        float cameraFramerate = VideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
-        Debug.Log(cameraFramerate);
-
-        VideoCapture.CreateAsync(false, delegate (VideoCapture videoCapture)
-        {
-            if (videoCapture != null)
-            {
-                this.videoCapture = videoCapture;
-                Debug.Log("Created VideoCapture Instance!");
-
-                CameraParameters cameraParameters = new CameraParameters();
-                cameraParameters.hologramOpacity = 0.0f;
-                cameraParameters.frameRate = cameraFramerate;
-                cameraParameters.cameraResolutionWidth = cameraResolution.width;
-                cameraParameters.cameraResolutionHeight = cameraResolution.height;
-                cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-
-                this.videoCapture.StartVideoModeAsync(cameraParameters,
-                                                   VideoCapture.AudioState.ApplicationAndMicAudio,
-                                                   OnStartedVideoCaptureMode);
-            }
-            else
-            {
-                Debug.LogError("Failed to create VideoCapture Instance!");
-            }
-        });
+        Texture2D texture2D = new Texture2D(physicalWebCamTexture.width, physicalWebCamTexture.height);
+        texture2D.SetPixels(physicalWebCamTexture.GetPixels());
+        texture2D.Apply();
+        //System.IO.File.WriteAllBytes("image.png", texture2D.EncodeToPNG());
     }
-
-    private void OnStartedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Started Video Capture Mode!");
-        string timeStamp = Time.time.ToString().Replace(".", "").Replace(":", "");
-        string filename = string.Format("TestVideo_{0}.mp4", timeStamp);
-        string filepath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-        filepath = filepath.Replace("/", @"\");
-        this.videoCapture.StartRecordingAsync(filepath, OnStartedRecordingVideo);
-    }
-
-    void OnStoppedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Stopped Video Capture Mode!");
-    }
-
-    void OnStartedRecordingVideo(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Started Recording Video!");
-        m_stopRecordingTimer = Time.time + MaxRecordingTime;
-    }
-
-    void OnStoppedRecordingVideo(VideoCapture.VideoCaptureResult result)
-    {
-        Debug.Log("Stopped Recording Video!");
-        videoCapture.StopVideoModeAsync(OnStoppedVideoCaptureMode);
-    }
-
-
-
 
 }
-*/
+
